dd11bc639c7a19ab681da9cadf0b144d413bab10
DinaraGilmanova <dinara-gilmanova@mail.ru>
2019-07-31 04:01:25 +0400

    excellent

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var currentData = [
+const currentData = [
   {
     date: 1564488000000,
     temperature: {
@@ -16,7 +16,7 @@ var currentData = [
       day: 29
     },
     cloudiness: "Облачно",
-    snow: true,
+    snow: false,
     rain: false
   },
   {
@@ -40,7 +40,17 @@ var currentData = [
     rain: false
   },
   {
-    date: 1564747200000,
+    date: 1564833600000,
+    temperature: {
+      night: 16,
+      day: 26
+    },
+    cloudiness: "Ясно",
+    snow: false,
+    rain: false
+  },
+  {
+    date: 1564920000000,
     temperature: {
       night: 16,
       day: 26
@@ -50,13 +60,13 @@ var currentData = [
     rain: false
   }
 ];
-var d = new Date();
+const d = new Date();
 d.setHours(16, 0, 0, 0);
-var n = d.getTime();
-var arry = [];
-for (var i = 0; i < currentData.length; i++) {
+const n = d.getTime();
+arry = [];
+for (let i = 0; i < currentData.length; i++) {
   if (n == currentData[i].date) {
-    var currentDay = currentData[i].date;
+    let currentDay = currentData[i].date;
     var nextDay = currentDay;
     for (k = 1; k < 4; k++) {
       nextDay += 24 * 60 * 60 * 1000;
@@ -73,56 +83,48 @@ for (var i = 0; i < currentData.length; i++) {
           "," +
           [nextDay] +
           ",";
-
-        console.log(currentData[k].date);
       }
     }
   }
 }
-console.log(arry);
-var array = arry.split(",").map(Number);
-console.log(array);
-//console.log(nextDay+= );
-
-var makeElement = function(tagName, className, text) {
-  var element = document.createElement(tagName);
+const array = arry.split(",").map(Number);
+const makeElement = function(tagName, className, text) {
+  const element = document.createElement(tagName);
   element.classList.add(className);
   if (text) {
     element.textContent = text;
   }
   return element;
 };
-var renderCards = function(weatherDay) {
-  var listItem = makeElement("li", "day");
+const renderCards = function(weatherDay) {
+  const listItem = makeElement("li", "day");
 
-  var optionsDate = { weekday: "long", month: "long", day: "numeric" };
-  var today = new Date(weatherDay.date);
-  weatherDay.date = today.toLocaleDateString("ru", optionsDate);
-  var date = makeElement("h2", "weather--h2", weatherDay.date);
+  const optionsDate = { weekday: "long", month: "long", day: "numeric" };
+  const toDay = new Date(weatherDay.date);
+  weatherDay.date = toDay.toLocaleDateString("ru", optionsDate);
+  const date = makeElement("h2", "weather--h2", weatherDay.date);
   listItem.appendChild(date);
 
   if (weatherDay.cloudiness === "Ясно") {
     weatherDay.snow = false;
     weatherDay.rain = false;
   }
-  var weatherImage = {
-    snow: "file:///D:/OSPanel/domains/weather/images/snow.svg",
-    rain: "file:///D:/OSPanel/domains/weather/images/rain.svg",
-    clouds: "file:///D:/OSPanel/domains/weather/images/clouds.svg",
-    sun: "file:///D:/OSPanel/domains/weather/images/sun.svg"
+
+  const weatherImage = {
+    snow: "file:///D:/weather/images/snow.svg",
+    rain: "file:///D:/weather/images/rain.svg",
+    clouds: "file:///D:/weather/images/clouds.svg",
+    sun: "file:///D:/weather/images/sun.svg"
   };
-  var picture = makeElement("img", "weather__image");
+  const picture = makeElement("img", "weather__image");
   if (weatherDay.cloudiness === "Облачно") {
     picture.src = weatherImage.clouds;
-    //picture.alt = product.text;
   }
   if (weatherDay.snow) {
     picture.src = weatherImage.snow;
-    //picture.alt = product.text;
   }
   if (weatherDay.rain) {
     picture.src = weatherImage.rain;
-    //picture.alt = product.text;
   }
   if (weatherDay.cloudiness === "Ясно") {
     picture.src = weatherImage.sun;
@@ -130,44 +132,29 @@ var renderCards = function(weatherDay) {
 
   listItem.appendChild(picture);
 
-  //Условие:если облачно - такая-то картинка и т.д
-  //var picture = makeElement('img', 'good__image');
-  //picture.src = product.imgUrl;
-  //picture.alt = product.text;
-  //listItem.appendChild(picture);
-  var temperatureDay1 = document.createElement("p");
-  //console.log('-----', weatherDay);
- temperatureDay1.innerHTML = "днём: " +
- weatherDay.temperature.day +
- "℃" +
- " ночью: " +
- weatherDay.temperature.night +
- "℃";
+  const temperatureDay = document.createElement("p");
+  temperatureDay.classList.add("weather--temperature");
+  temperatureDay.innerHTML =
+    "днём: " +
+    weatherDay.temperature.day +
+    "℃  " +
+    " ночью: " +
+    weatherDay.temperature.night +
+    "℃";
 
-//   var temperatureDay = makeElement(
-//     "p",
-//     "weather--p",
-//     "днём: " +
-//       currentData[i].temperature.day +
-//       "℃" +
-//       " ночью: " +
-//       currentData[i].temperature.night +
-//       "℃"
-//   );
+  listItem.appendChild(temperatureDay);
 
-  listItem.appendChild(temperatureDay1);
-
-  var precipitation;
+  let precipitation;
   if (!weatherDay.snow || !weatherDay.rain) {
     precipitation = "без осадков";
   }
   if (weatherDay.snow) {
-    precipitation = "снег";
+    precipitation = "возможен снег";
   }
   if (weatherDay.rain) {
-    precipitation = "дождь";
+    precipitation = "возможен дождь";
   }
-  var options = makeElement(
+  const options = makeElement(
     "p",
     "weather--p",
     weatherDay.cloudiness + ", " + precipitation
@@ -178,10 +165,34 @@ var renderCards = function(weatherDay) {
   return listItem;
 };
 
-var cardList = document.querySelector(".days");
+const cardList = document.querySelector(".days");
 
-for (var i = 0; i < array.length - 1; i++) {
-  var cardItem = renderCards(currentData[i]);
+for (let i = 1; i < array.length; i++) {
+  let cardItem = renderCards(currentData[i]);
   cardList.appendChild(cardItem);
 }
-console.log(renderCards(currentData));
+// console.log(cardList.scrollLeft);
+// console.log(cardList.scrollX);
+
+const buttonLeft = document.querySelector(".left");
+const buttonRight = document.querySelector(".right");
+
+const step = document.querySelector(".day").clientWidth;
+
+buttonLeft.onclick = function() {
+  cardList.scrollLeft = 0;
+};
+
+buttonRight.onclick = function() {
+  cardList.scrollLeft = step;
+};
+
+cardList.onscroll = function() {
+  if (cardList.scrollLeft === step) {
+    buttonLeft.disabled = false;
+    buttonRight.disabled = true;
+  } else {
+    buttonLeft.disabled = true;
+    buttonRight.disabled = false;
+  }
+};
